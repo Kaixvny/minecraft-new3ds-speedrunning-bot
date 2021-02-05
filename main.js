8be54b7cd073a4f4e0bc5e70b9983c6a1fc6d64d
Kaixvny <68577140+Kaixvny@users.noreply.github.com>
2021-02-05 18:54:34 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const client = new discord.Client();
 const prefix =  'n3ds!';
 
 client.on('ready', () => {
- console.log('N3DS Bot is online! LETS GOOOOOO!')
+ console.log('ONLINE! NO ERRORS FOUND!')
 });
 
 client.once('ready', () => { client.user.setActivity('https://www.speedrun.com/mc3ds | n3ds!', { type: "WATCHING" }); });
@@ -16,11 +16,9 @@ client.on('message', message =>{
     const args = message.content.slice(prefix.length).split(/ +/)
     const command = args.shift().toLowerCase();
 
-    if(command === 'ss'){
-    message.channel.send('Any% - Set Seed - 9m 54s 033ms - By "blunderpolicy" \n Seed: 66898262 \n Run: https://www.speedrun.com/mc3ds/run/yd90epxz')
-    }
+    
 
-    if(command === 'ss'){
+if(command === 'ss'){
         message.channel.send({embed: {
             "title": "Any% - Set Seed - 9m 54s 033ms - By \"blunderpolicy\"",
             "description": "**Seed:** 66898262\n\n[Run on speedrun.com](https://www.speedrun.com/mc3ds/run/yd90epxz)",
